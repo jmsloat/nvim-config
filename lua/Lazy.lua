e91c37b615a2e9e3f73157fce8bb375b512747bf
James Sloat <jsloat@google.com>
2023-07-10 14:09:57 +0000

    remove debug

--- lua/Lazy.lua
+++ lua/Lazy.lua
@@ -20,7 +20,7 @@ require("lazy").setup("user", {
   defaults = { lazy = true, version = "57cce98dfdb2f2dd05a0567d89811e6d0505e13b" },
   ui = { wrap = "true" },
   change_detection = { enabled = true, notify = false },
-  debug = true,
+  debug = false,
   performance = {
     rtp = {
       disabled_plugins = {
