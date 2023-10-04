e82e2435bf04bd866a91fbfc1cb20decfb6ee9b9
James Sloat <jsloat@google.com>
2023-10-04 13:20:33 +0000

    treesitter update

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -1,6 +1,6 @@
 local M = {
   "nvim-treesitter/nvim-treesitter",
-  commit = "226c1475a46a2ef6d840af9caa0117a439465500",
+  commit = "eb96ca52a7e72ec6d45b163e90bcb5e0774d44ad",
   event = "BufReadPost",
   dependencies = {
     {
